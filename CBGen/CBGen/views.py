ca2736b53e1d154ceca9b5a50dcb713d2dc47e41
lolomap <lolomap07@yandex.ru>
2021-07-11 12:13:46 +0300

    Flag generation update
    horizontal and vertical lining added
    little fixes in the element generation
    title changed

--- CBGen/CBGen/views.py
+++ CBGen/CBGen/views.py
@@ -32,8 +32,8 @@ CountryGovtypes = {0: ["Абсолютная монархия", "Дуалист�
                    3: ["Президент", "Парламент", "Президент и парламент"],
                    4: ["Военная диктатура", "Вождизм"]}
 
-FlagTemplates = ['cross', 'cross_stand', 'plus', 'star', 'circle', 'horizontal', 'vertical', 'div_block', 'div', 'div_db',
-                 'random']
+FlagTemplates = ['cross', 'cross_stand', 'plus', 'star', 'circle', 'div_block', 'random']
+BGTemplates = ['horizontal', 'vertical', 'blank']
 
 def name_create():
     result = ''
@@ -66,7 +66,7 @@ def name_create():
                     result += glas[random.randint(0, len(glas) - 1)]
     
     if len(result) < 6:
-        if random.randint(1, 10) < 2:
+        if random.randint(1, 10) < 5:
             if random.randint(0, 1) == 1:
                 result += glas_soed[random.randint(0, len(glas_soed) - 1)]
             result += CountryPostfixes[random.randint(0, len(CountryPostfixes) - 1)]
@@ -77,7 +77,7 @@ def cross_stand_paint(img, shape, start):
     x = random.randint(start[0], shape[1])
     y = random.randint(start[1], shape[0])
     color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-    thickness = random.randint(20, 70)
+    thickness = random.randint(shape[0] // 25.6, shape[0] // 7.3)
     cv2.line(img, (x, start[1]), (x, shape[0]), color, thickness)
     cv2.line(img, (start[0], y), (shape[1], y), color, thickness)
 
@@ -85,7 +85,7 @@ def cross_paint(img, shape, start):
     x = random.randint(start[0], shape[1])
     y = random.randint(start[1], shape[0])
     color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-    thickness = random.randint(20, 70)
+    thickness = random.randint(shape[0] // 25.6, shape[0] // 7.3)
     angle = math.radians(random.randint(10, 70))
 
     l1top = int(x + y // math.tan(angle))
@@ -112,52 +112,93 @@ def div_block_paint(img, shape, start):
         cv2.rectangle(img, (start[0], start[1]), (x, y), color, -1)
         fs = (0, 0)
     elif oriantation == 2:
-        cv2.rectangle(img, (shape[1] - x, start[1]), (start[0], y), color, -1)
+        cv2.rectangle(img, (shape[1] - x, start[1]), (shape[1], y), color, -1)
         fs = (shape[1] - x, start[1])
     elif oriantation == 3:
         cv2.rectangle(img, (shape[1] - x, shape[0] - y), (shape[1], shape[0]), color, -1)
         fs = (shape[1] - x, shape[0] - y)
     elif oriantation == 4:
-        cv2.rectangle(img, (start[0], shape[0] - y), (x, start[1]), color, -1)
+        cv2.rectangle(img, (start[0], shape[0] - y), (x, shape[1]), color, -1)
         fs = (start[0], shape[0] - y)
     fsh = (fs[1] + y, fs[0] + x)
     return (fs, fsh)
 
-def paint(img):
+def paint(img, shape, start):
     templates_num = random.randint(1, 3)
-    flag_shape = img.shape
-    flag_start = (0, 0)
+    
     for i in range(templates_num):
         templ = FlagTemplates[random.randint(0, len(FlagTemplates) - 1)]
         if templ == 'cross_stand':
-            cross_stand_paint(img, flag_shape, flag_start)
+            cross_stand_paint(img, shape, start)
         elif templ == 'cross':
-            cross_paint(img, flag_shape, flag_start)
+            cross_paint(img, shape, start)
         elif templ == 'circle':
-            circle_paint(img, flag_shape, flag_start)
+            circle_paint(img, shape, start)
         elif templ == 'div_block':
-            flag_start, flag_shape = div_block_paint(img, flag_shape, flag_start)
+            start, shape = div_block_paint(img, shape, start)
         else:
             continue
+
+def bgpaint(img, shape, start):
+    bg = BGTemplates[random.randint(0, len(BGTemplates) - 1)]
+    line_count = random.randint(1, 10)
+    if bg == 'horizontal':
+        y_step = shape[0] // line_count
+        coltype = random.randint(0, 1)
+        if line_count > 2:
+            if random.randint(1, 10) < 8:
+                coltype = 0
+        c = [(0, 0, 0), (0, 0, 0)]
+        if coltype == 0:
+            c[0] = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+            c[1] = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+            for i in range(line_count):
+                color = c[i % 2]
+                cv2.rectangle(img, (start[0], start[1]), (shape[1], start[1] + y_step), color, -1)
+                start = (start[0], start[1] + y_step)
+        else:
+            for i in range(line_count):
+                color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+                cv2.rectangle(img, (start[0], start[1]), (shape[1], start[1] + y_step), color, -1)
+                start = (start[0], start[1] + y_step)
+    elif bg == 'vertical':
+        x_step = shape[1] // line_count
+        coltype = random.randint(0, 1)
+        if line_count > 2:
+            if random.randint(1, 10) < 8:
+                coltype = 0
+        c = [(0, 0, 0), (0, 0, 0)]
+        if coltype == 0:
+            c[0] = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+            c[1] = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+            for i in range(line_count):
+                color = c[i % 2]
+                cv2.rectangle(img, (start[0], start[1]), (start[0] + x_step, shape[0]), color, -1)
+                start = (start[0] + x_step, start[1])
+        else:
+            for i in range(line_count):
+                color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+                cv2.rectangle(img, (start[0], start[1]), (start[0] + x_step, shape[0]), color, -1)
+                start = (start[0] + x_step, start[1])
 
 def flag_create():
     color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
     img = np.full((512, 512, 3), color, np.uint8)
-    print('paint start')
-    paint(img)
-    print('paint finnished')
+    flag_shape = img.shape
+    flag_start = (0, 0)
+    bgpaint(img, flag_shape, flag_start)
+    if random.randint(1, 10) < 7:
+        paint(img, flag_shape, flag_start)
     fn = str(time.time())
     is_success, buffer = cv2.imencode('.jpg', img)
     fio = io.BytesIO(buffer)
     fio.seek(0)
     url = base64.b64encode(fio.getvalue()).decode()
-    print('file saved')
     return 'data:image/jpeg;base64, {}'.format(url)
 
 @app.route("/", methods=["POST"])
 def generate():
     try:
-        print('generation started')
         name = ''
 
         rideo = random.randint(0, len(CountryIdeologies) - 1)
@@ -188,10 +229,8 @@ def generate():
         name += rtype
         name += ' ' + name_create()
 
-        print('flag generation started')
         flag = flag_create()
 
-        print('done')
         return render_template('index.html', title='Home Page', BallFlag = flag,
                                BallName=name, BallIdeology=ideology, BallGovtype=govtype)
     except Exception as e:
